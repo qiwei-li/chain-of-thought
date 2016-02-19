5401bca4ff619b0cb304b22c988ec2e6681ec234
Qiwei <liqiwei1224@gmail.com>
2016-02-18 16:34:39 -0800

    added try

--- basic.py
+++ basic.py
@@ -1,3 +1,5 @@
+import time
+
 def basic(inputIDs):
     import re
     import requests
@@ -15,20 +17,33 @@ def basic(inputIDs):
         r = requests.get(url)
         soup = BeautifulSoup(r.content)
 
-        title = soup.find('div', {'class': 'title'}).text.replace('\n', '').replace('\t', '')
+        try:
+            title = soup.find('div', {'class': 'title'}).text.replace('\n', '').replace('\t', '')
+        except:
+            title = 'NA'
         titles.append(title)
 
-        author = soup.find_all('span', {'id': 'preferredName'})
-        tmp = []
-        for item in author:
-            tmp.append(str(item).replace('<span class="','').replace('" id="preferredName"></span>',''))
+        try:
+            author = soup.find_all('span', {'id': 'preferredName'})
+            tmp = []
+            for item in author:
+                tmp.append(str(item).replace('<span class="','').replace('" id="preferredName"></span>',''))
+        except:
+            author = 'NA'
         authors.append('; '.join(list(set(tmp))))
 
-        dates.append(str(soup.find_all('dl')[1]).split('Date')[1].replace('\n','').replace('\t','').split('<dd>')[1].split('</dd>')[0])
+        try:
+            date = str(soup.find_all('dl')[1]).split('Date')[1].replace('\n','').replace('\t','').split('<dd>')[1].split('</dd>')[0]
+        except:
+            date = 'NA'
+        dates.append(date)
 
     return [ids, titles, authors, dates]
 
 
-id = [4492360, 4492364]
+id = range(4492360, 4492370)
+start_time = time.time()
 ans = basic(id)
+end_time = time.time()
 print(ans)
+print end_time - start_time
